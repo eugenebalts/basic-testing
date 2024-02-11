12cc14cc4311972f459a8271925664bc28fc6e17
Yauheni Baltsevich <norcobeatz@gmail.com>
2024-02-11 22:54:20 +0100

    test: implement table-tests

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,31 @@
-// Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+  { a: 1, b: 2, action: Action.Add, expected: 3 },
+  { a: 2, b: 2, action: Action.Add, expected: 4 },
+  { a: 3, b: 2, action: Action.Add, expected: 5 },
+  { a: 4, b: 2, action: Action.Divide, expected: 2 },
+  { a: 5, b: 2, action: Action.Divide, expected: 2.5 },
+  { a: 6, b: 2, action: Action.Divide, expected: 3 },
+  { a: 7, b: 2, action: Action.Exponentiate, expected: 49 },
+  { a: 8, b: 2, action: Action.Exponentiate, expected: 64 },
+  { a: 9, b: 2, action: Action.Exponentiate, expected: 81 },
+  { a: 10, b: 2, action: Action.Multiply, expected: 20 },
+  { a: 11, b: 3, action: Action.Multiply, expected: 33 },
+  { a: 12, b: 4, action: Action.Multiply, expected: 48 },
+  { a: 13, b: 2, action: Action.Subtract, expected: 11 },
+  { a: 14, b: 2, action: Action.Subtract, expected: 12 },
+  { a: 15, b: 2, action: Action.Subtract, expected: 13 },
+];
 
 describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
+  testCases.forEach((suite) => {
+    const { a, b, action, expected } = suite;
+
+    const testSuite = simpleCalculator({ a, b, action });
+
+    test(`should return ${expected} with argument a:${a}, b:${b} and action=${action}`, () => {
+      expect(testSuite).toBe(expected);
+    });
   });
-  // Consider to use Jest table tests API to test all cases above
 });
